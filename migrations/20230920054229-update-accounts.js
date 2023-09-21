be72dc22cf5f6b371cad774fd8027cc5c55d1bef
SankaraNarayanan-Baskaran <145688295+SankaraNarayanan-Baskaran@users.noreply.github.com>
2023-09-21 19:51:35 +0530

    Add files via upload

---
+++ migrations/20230920054229-update-accounts.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { query } = require('express');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.changeColumn('Accounts','username',{
+      type:Sequelize.STRING,
+      allowNull:false
+    }),
+    await queryInterface.changeColumn('Accounts','password',{
+      type:Sequelize.STRING,
+      allowNull:false
+    }),
+    await queryInterface.changeColumn('Accounts','email',{
+      type:Sequelize.STRING,
+      allowNull:false
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
